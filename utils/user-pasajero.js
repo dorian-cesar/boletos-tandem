87167e805b5e8f1330f98d5ade1789efb683da4d
Jose Candia <josecandiap@gmail.com>
2023-04-03 15:58:52 -0400

    se agregan valores faltantes al metodo validar

--- utils/user-pasajero.js
+++ utils/user-pasajero.js
@@ -1,4 +1,8 @@
-export function isValidPasajero(pasajero, indexPasajero, direccionRecorrido, setCarro) {
+import Rut from 'rutjs';
+
+const isSame = (array1, array2) => array1.length === array2.length && array1.every((value, index) => value === array2[index]);
+
+export function isValidPasajero(pasajero, indexPasajero, direccionRecorrido, setCarro, carro) {
     try {
         let isValid = true;
         let errors = [...pasajero.errors];
