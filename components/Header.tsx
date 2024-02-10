1ecc17959056e329fb6b904288d4e9ca1b433b14
Jose Candia <jose.candia@fusiona.cl>
2024-02-10 02:47:25 -0300

    solucion error carro de compras en header

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,11 +12,12 @@ export default function Header({ openNav }: { openNav: any }) {
   const [user, setUser] = useState();
   const router = useRouter();
   const { getItem, clear } = useLocalStorage();
-
-  const carroCompras = useSelector((state:any) => state.compra?.listaCarrito) || []
+  const [carroCompras, setCarroCompras] = useState([]);
+  const data = useSelector((state:any) => state.compra?.listaCarrito) || [];
   
   useEffect(() => {
     setUser(getItem("user"));
+    setCarroCompras(data);
   }, []);
 
   return (
@@ -116,14 +117,14 @@ export default function Header({ openNav }: { openNav: any }) {
                     </li>
                   </ul>
                 )}
-                {/* <Link href="/carrito" legacyBehavior>
+                <Link href="/carrito" legacyBehavior>
                   <a className="d-flex align-items-center">
                     <img src="../img/cart-outline.svg" width={30} />
                     <span className="badge bg-primary rounded-pill">
                       {carroCompras.length}
                     </span>
                   </a>
-                </Link> */}
+                </Link>
               </div>
             </div>
           </div>
