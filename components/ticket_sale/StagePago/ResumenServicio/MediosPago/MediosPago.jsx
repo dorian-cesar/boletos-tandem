0a1e9ef80f586808b8d31fe1466b4256dfa940b7
Julio Villagra <jvillagra@pullman.cl>
2024-02-22 14:23:54 -0300

    Implementacion diseño componente medio pago y servicio obtener medios pago

--- components/ticket_sale/StagePago/ResumenServicio/MediosPago/MediosPago.jsx
+++ components/ticket_sale/StagePago/ResumenServicio/MediosPago/MediosPago.jsx
@@ -1,15 +1,75 @@
-import styles from "./MediosPago.module.css"
+import { useEffect, useRef, useState } from "react";
+import styles from "./MediosPago.module.css";
+import axios from "axios";
 
-const PuntoEmbarque = (props) => {
+const MediosPago = (props) => {
+  const [mediosPago, setMediosPago] = useState([]);
+  const [selectedMedioPago, setSelectedMedioPago] = useState(null); // no se si los podemos controlar con el store para pagar
+  const [codigoCuponera, setCodigoCuponera] = useState(""); // lo mismo cuando seleccionen cuponera guardar esos datos en el store para hacer validaciones
+  const handleMedioPagoChange = (id) => {
+    setSelectedMedioPago(id);
+  };
+
+  useEffect(
+    () =>
+      async function obtenerMediosPagos() {
+        const { data } = await axios.post(
+          "/api/ticket_sale/obtener-medios-pago",
+          {}
+        );
+        setMediosPago(data);
+      },
+    []
+  );
+
   return (
     <>
       <div className={styles["container"]}>
-        <div className={styles["dotted-line"]}>
-            ponga sus medios de pago aqui, falta servicio 
-        </div>
+        {mediosPago.map((element) => (
+          <div key={element.id} className={styles["body-pay"]}>
+            <input
+              type="radio"
+              id={element.id}
+              name="medioPago"
+              value={element.valor2}
+              checked={selectedMedioPago === element.valor2}
+              onChange={() => handleMedioPagoChange(element.valor2)}
+            />
+            {element.valor2 === "WBPAY" ? (
+              <img src="/img/icon/cuponera/Logo-webpay.svg"></img>
+            ) : (
+              <label
+                className={
+                  element.valor2 === "CUP" ? styles["text-coupon"] : ""
+                }
+                htmlFor={element.id}
+              >
+                {element?.valor1}
+                <img src="/img/icon/cuponera/ticket-outline.svg"></img>
+              </label>
+            )}
+          </div>
+        ))}
+
+        {selectedMedioPago === "CUP" ? (
+          <>
+            <span className={styles["text-input-coupon"]}>
+              {" "}
+              Ingresa tu código de transacción aquí:{" "}
+            </span>
+            <input
+              type="codigoCuponera"
+              value={codigoCuponera}
+              name="codigoCuponera"
+              placeholder="Ej: XTQ1234567"
+              className={styles["input-body"]}
+              onChange={(e) => setCodigoCuponera(e.target.value)}
+            />
+          </>
+        ) : null}
       </div>
     </>
   );
 };
 
-export default PuntoEmbarque;
+export default MediosPago;
