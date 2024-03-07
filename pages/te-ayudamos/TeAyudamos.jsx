0a8e20e089e40f28d643769abdc4abd3ad4b2bff
Jose Candia <josecandiap@gmail.com>
2024-03-07 14:49:41 -0300

    ajuste de pantalla te ayudamos

--- pages/te-ayudamos/TeAyudamos.jsx
+++ pages/te-ayudamos/TeAyudamos.jsx
@@ -22,102 +22,104 @@ const TeAyudamos = (props) => {
         <div className={styles["body"]}>
           <div className={styles["title"]}>Nuestro canales de atención</div>
           <div className="container">
-            <div className="d-flex justify-content-center">
-              <div className="col-12 col-md-5">
-                <div
-                  className={`${styles["question-body"]} "d-flex justify-content-center" `}
-                >
-                  <div className={styles["image"]}>
-                    <img src="/img/icon/help/help-circle-outline.svg"></img>
+            <div className="row">
+              <div className="d-flex row justify-content-center">
+                <div className="col-12 col-md-5">
+                  <div
+                    className={`${styles["question-body"]} "d-flex justify-content-center" `}
+                  >
+                    <div className={styles["image"]}>
+                      <img src="/img/icon/help/help-circle-outline.svg"></img>
+                    </div>
+                    <div className={styles["title-message"]}>
+                      Preguntas frecuentes
+                    </div>
+                    <div className={styles["message"]}>
+                      Aquí tienes un montón de respuestas a las preguntas que la
+                      gente suele hacer. Échale un vistazo y hagamos que todo sea
+                      más fácil para ti!
+                    </div>
+                    <div className={""}>
+                      <div
+                        className={styles["button"]}
+                        onClick={() => {
+                          cambiarVista(1);
+                        }}
+                      >
+                        Encuentra respuestas aquí
+                      </div>
+                    </div>
                   </div>
-                  <div className={styles["title-message"]}>
-                    Preguntas frecuentes
-                  </div>
-                  <div className={styles["message"]}>
-                    Aquí tienes un montón de respuestas a las preguntas que la
-                    gente suele hacer. Échale un vistazo y hagamos que todo sea
-                    más fácil para ti!
-                  </div>
-                  <div className={""}>
-                    <div
-                      className={styles["button"]}
-                      onClick={() => {
-                        cambiarVista(1);
-                      }}
-                    >
-                      Encuentra respuestas aquí
+                </div>
+                <div className="col-12 col-md-5">
+                  <div
+                    className={`${styles["question-body"]} "d-flex justify-content-center" `}
+                  >
+                    <div className={styles["image"]}>
+                      <img src="/img/icon/help/megaphone-outline.svg"></img>
+                    </div>
+                    <div className={styles["title-message"]}>Escríbenos</div>
+                    <div className={styles["message"]}>
+                      ¿No hallaste lo que necesitabas en nuestra sección de
+                      preguntas frecuentes? Mándanos un mensaje y nos pondremos en
+                      acción para responderte lo más pronto posible.
+                    </div>
+                    <div className={""}>
+                      <div
+                        className={styles["button"]}
+                        onClick={() => {
+                          cambiarVista(2);
+                        }}
+                      >
+                        Habla con Nosotros
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-12 col-md-5">
-                <div
-                  className={`${styles["question-body"]} "d-flex justify-content-center" `}
-                >
-                  <div className={styles["image"]}>
-                    <img src="/img/icon/help/megaphone-outline.svg"></img>
-                  </div>
-                  <div className={styles["title-message"]}>Escríbenos</div>
-                  <div className={styles["message"]}>
-                    ¿No hallaste lo que necesitabas en nuestra sección de
-                    preguntas frecuentes? Mándanos un mensaje y nos pondremos en
-                    acción para responderte lo más pronto posible.
-                  </div>
-                  <div className={""}>
-                    <div
-                      className={styles["button"]}
-                      onClick={() => {
-                        cambiarVista(2);
-                      }}
-                    >
-                      Habla con Nosotros
+              <div className="d-flex row justify-content-center">
+                <div className="col-12 col-md-5">
+                  <div className={`${styles["other-body"]}  `}>
+                    <div className={styles["title-contact"]}>
+                      Tambien puedes contactarnos a:
+                    </div>
+                    <div className={"row"}>
+                      <div className={"col-12"}>
+                        <img
+                          className={styles["image-icon"]}
+                          src="/img/icon/help/call-outline.svg"
+                        ></img>
+                        600 600 0018
+                      </div>
+                    </div>
+                    <div className={"row"}>
+                      <div className={"col-12"}>
+                        <img
+                          className={styles["image-icon"]}
+                          src="/img/icon/help/what-up.svg"
+                        ></img>
+                        +56233048912
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="d-flex justify-content-center">
-              <div className="col-12 col-md-5">
-                <div className={`${styles["other-body"]}  `}>
-                  <div className={styles["title-contact"]}>
-                    Tambien puedes contactarnos a:
-                  </div>
-                  <div className={"row"}>
-                    <div className={"col-12"}>
-                      <img
-                        className={styles["image-icon"]}
-                        src="/img/icon/help/call-outline.svg"
-                      ></img>
-                      600 600 0018
+                <div className="col-12 col-md-5">
+                  <div className={`${styles["other-body"]}  `}>
+                    <div className={styles["title-contact"]}>
+                      Horario de atención:
                     </div>
-                  </div>
-                  <div className={"row"}>
-                    <div className={"col-12"}>
-                      <img
-                        className={styles["image-icon"]}
-                        src="/img/icon/help/what-up.svg"
-                      ></img>
-                      +56233048912
+                    <div className={"row"}>
+                      <div className={"col-12"}>
+                        <span className={styles["text-bold"]}>SAC</span> | Lunes a
+                        viernes: 09:00AM - 18:00PM
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-12 col-md-5">
-                <div className={`${styles["other-body"]}  `}>
-                  <div className={styles["title-contact"]}>
-                    Horario de atención:
-                  </div>
-                  <div className={"row"}>
-                    <div className={"col-12"}>
-                      <span className={styles["text-bold"]}>SAC</span> | Lunes a
-                      viernes: 09:00AM - 18:00PM
-                    </div>
-                  </div>
-                  <div className={"row"}>
-                    <div className={"col-12"}>
-                      <span className={styles["text-bold"]}>Call Center</span> |
-                      Lunes a viernes: 09:00AM - 18:00PM <br />
-                      Sábado, domingos y festivos: 09:00AM - 19:00PM
+                    <div className={"row"}>
+                      <div className={"col-12"}>
+                        <span className={styles["text-bold"]}>Call Center</span> |
+                        Lunes a viernes: 09:00AM - 18:00PM <br />
+                        Sábado, domingos y festivos: 09:00AM - 19:00PM
+                      </div>
                     </div>
                   </div>
                 </div>
