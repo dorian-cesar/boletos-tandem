8a4469647832a73a7fd4144d3baaa1e7bca8eef4
roger.rodriguez@sertran.cl <roger.rodriguez@sertran.cl>
2024-04-04 11:48:40 -0300

    se modifica ventana te ayudamos  a responsive

--- pages/te-ayudamos/TeAyudamos.jsx
+++ pages/te-ayudamos/TeAyudamos.jsx
@@ -85,7 +85,7 @@ const TeAyudamos = (props) => {
                     </div>
                     <div className={"row"}>
                       <div className={"col-12"}>
-                        <a className={ styles['link-contacto'] } href="tel:26006000018">
+                        <a className={styles['link-contacto']} href="tel:26006000018">
                           <img
                             className={styles["image-icon"]}
                             src="/img/icon/help/call-outline.svg"
@@ -96,7 +96,7 @@ const TeAyudamos = (props) => {
                     </div>
                     <div className={"row"}>
                       <div className={"col-12"}>
-                        <a className={ styles['link-contacto'] } href="https://wa.me/56233048912" target="_blank">
+                        <a className={styles['link-contacto']} href="https://wa.me/56233048912" target="_blank">
                           <img
                             className={styles["image-icon"]}
                             src="/img/icon/help/what-up.svg"
