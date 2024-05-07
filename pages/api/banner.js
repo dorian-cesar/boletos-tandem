bef46b719f7f695a2302dacbde4d3e225d14397e
Jose Candia <josecandiap@gmail.com>
2024-05-07 10:32:58 -0400

    se agrega metodo para obtener banners

---
+++ pages/api/banner.js
@@ -0,0 +1,21 @@
+import doLogin from '../../utils/oauth-token';
+import getConfig from 'next/config'
+import axios from "axios"
+const {serverRuntimeConfig} = getConfig();
+const config = serverRuntimeConfig;
+
+export default async (req, res) => {
+
+    try {
+        let token = await doLogin();
+        let data = await axios.get(config.service_url + `/parametros/obtenerBannerPortal`,{
+            headers: {
+                'Authorization': `Bearer ${token.token}`
+            }
+        })
+        res.status(200).json(data.data);
+    } catch(e){
+        res.status(400).json(e.response.data)
+    }
+    
+}   
