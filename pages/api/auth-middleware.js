c8c360bdfa441d06ddcc4c42eca77989694004be
Jose Candia <josecandiap@gmail.com>
2024-06-01 21:24:38 -0400

    hotfix: mas ajustes de seguridad XD

--- pages/api/auth-middleware.js
+++ pages/api/auth-middleware.js
@@ -1,9 +1,17 @@
 import { verifyToken } from 'utils/jwt-auth';
 
+const allowedOrigins = ['http://localhost:3000', 'https://www.pullman.cl', 'http://128.1.0.190', 'http://128.1.0.191'];
+
 export const authMiddleware = handler => async (req, res) => {
     try {
         const header = req.headers.authorization || req.headers.Authorization;
         const token = req.query.token;
+
+        const referer = req.headers.referer;
+
+        if( !referer || !allowedOrigins.some(d => referer.startsWith(d)) ) {
+            return res.status(401).json({ message: 'Not Allowed' });
+        }
 
         if (!header) {
             return res.status(401).json({ message: 'Authorization header missing' });
