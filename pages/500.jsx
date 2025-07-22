27725f22939386cf67fb76b2d302fb698862379b
diego <dwigodski@gmail.com>
2025-07-22 15:17:04 -0400

    chore: se mejoran estilos en botón de pág error 500

--- pages/500.jsx
+++ pages/500.jsx
@@ -8,7 +8,7 @@ export default function Custom500() {
             <img src='img/server_error_image.png' width={ 306 } height={ 228 }/>
             <span>¡Oops! hemos tenido problemas con nuestro servidor, estamos trabajando para solucionarlo.</span>
             <Link href={ '/' }>
-                <button className='btn'>
+                <button className='btn btn-primary'>
                     Regresar
                 </button>
             </Link>
