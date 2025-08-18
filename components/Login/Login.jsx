c46cce5f0a8e5d5af0451579bf25cc402149522b
diego <dwigodski@gmail.com>
2025-08-18 14:35:47 -0400

    fix: se modifica el componente login agregando formulario para aceptar input submit con la tecla enter

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -41,7 +41,6 @@ const Login = (props) => {
       setAlert({
         msg: "Rellene los campos vacios.",
         visible: true,
-        // type: "alert-danger",
         type: "text-danger",
       });
       setEmptyFields({
@@ -54,17 +53,8 @@ const Login = (props) => {
     try {
       console.log("Iniciando sesión...");
       setIsLoading(true);
-      // const res = await axios.post("/api/user/validar-login", { ...login });
-      // const { token, usuario } = res.data.object;
-
-      // console.log(usuario, typeof usuario.fechaNacimiento);
-
-      // Object.assign(usuario.wallet, { last_update: new Date() });
-
-      // console.log("USUARIO:::", usuario);
 
       const res = await axios.post("/api/user/validar-login", { ...login });
-
       const { token, user } = res.data;
 
       const nombresSplit = user.name.split(" ");
@@ -77,20 +67,6 @@ const Login = (props) => {
         rut: user.rut,
         correo: user.email,
       };
-
-      // const usuario = {
-      //   rut: "76.485.689-9",
-      //   apellidoMaterno: "Test",
-      //   apellidoPaterno: "Test",
-      //   correo: "dwigodski@wit.la",
-      //   correo2: "",
-      //   fechaNacimiento: "05/05/2000",
-      //   nombres: "Prueba",
-      //   tipoDocumento: "R",
-      //   sexo: "MASCULINO",
-      // };
-
-      // const token = "token_prueba";
 
       encryptData(userLogged, LocalStorageEntities.user_auth);
       encryptData(token, LocalStorageEntities.user_token);
@@ -107,14 +83,12 @@ const Login = (props) => {
         setAlert({
           msg: e.response.data.message,
           visible: true,
-          // type: "alert-danger",
           type: "text-danger",
         });
       } else {
         setAlert({
           msg: "Ocurrió un error inesperado.",
           visible: true,
-          type: "text-danger",
           type: "alert-danger",
         });
       }
@@ -138,128 +112,134 @@ const Login = (props) => {
           {mode == "0" ? (
             <div className="modal-content">
               <div className="modal-body">
-                <div className={`${styles["index-login"]} "container" `}>
-                  <div className="d-flex justify-content-center ">
-                    <img
-                      className={styles["foto-login"]}
-                      src="../../img/icon-foto.svg"
-                      width={65}
-                    ></img>
-                  </div>
-                  {/* <div className="d-flex justify-content-center mt-2">
-                    <h4 className="titulo-azul">¡Hola!</h4>
-                  </div> */}
-                  {/* <p className="d-flex justify-content-center fw-bold mb-5">
-                    Inicia sesión con tu correo electrónico
-                  </p> */}
-                  <p className={styles["viajemos-juntos"]}>¡Viajemos juntos!</p>
-                  <p className={styles["ingresa-sesion"]}>
-                    Ingresa a tu sesión
-                  </p>
-                  <p className={styles["parrafo-registro"]}>
-                    Al registrarte o iniciar sesión, estás aceptando nuestros
-                    términos y condiciones de uso.
-                  </p>
-                  <div className="row text-center">
-                    {alert.visible ? (
-                      <div className={"alert " + alert?.type} role="alert">
-                        {alert?.msg}
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    onLogin();
+                  }}
+                >
+                  <div className={`${styles["index-login"]} "container" `}>
+                    <div className="d-flex justify-content-center ">
+                      <img
+                        className={styles["foto-login"]}
+                        src="../../img/icon-foto.svg"
+                        width={65}
+                      ></img>
+                    </div>
+                    <p className={styles["viajemos-juntos"]}>
+                      ¡Viajemos juntos!
+                    </p>
+                    <p className={styles["ingresa-sesion"]}>
+                      Ingresa a tu sesión
+                    </p>
+                    <p className={styles["parrafo-registro"]}>
+                      Al registrarte o iniciar sesión, estás aceptando nuestros
+                      términos y condiciones de uso.
+                    </p>
+
+                    <div className="row text-center">
+                      {alert.visible ? (
+                        <div className={"alert " + alert?.type} role="alert">
+                          {alert?.msg}
+                        </div>
+                      ) : (
+                        ""
+                      )}
+                    </div>
+
+                    {isLoading ? (
+                      <div className="d-flex justify-content-center">
+                        <div
+                          className="spinner-border text-primary"
+                          role="status"
+                        >
+                          <span className="visually-hidden"></span>
+                        </div>
                       </div>
                     ) : (
                       ""
                     )}
-                  </div>
-                  {isLoading ? (
+
+                    {Object.keys(loginFormFields).map((key) => (
+                      <div className="row mt-2" key={key}>
+                        <div className="col-12">
+                          <label className="label-input">
+                            {key === "email"
+                              ? "Correo electrónico"
+                              : "Contraseña"}
+                          </label>
+                          <input
+                            type={key === "password" ? "password" : "text"}
+                            placeholder={
+                              key === "email"
+                                ? "Ej: example@example.com"
+                                : "Ej: ******"
+                            }
+                            className={
+                              "form-control" +
+                              (emptyFields[key] ? " is-invalid" : "")
+                            }
+                            name={key}
+                            value={login[key]}
+                            onChange={onInputChange}
+                          />
+                          {emptyFields[key] && (
+                            <div className="invalid-feedback">
+                              Campo obligatorio
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+
                     <div className="d-flex justify-content-center">
-                      <div
-                        className="spinner-border text-primary"
-                        role="status"
+                      <button
+                        type="button"
+                        className="btn-link btn-modal-link"
+                        onClick={() => changeMode("1")}
                       >
-                        <span clasNames="visually-hidden"></span>
-                      </div>
+                        ¿Olvidé mi password?
+                      </button>
                     </div>
-                  ) : (
-                    ""
-                  )}
-                  {Object.keys(loginFormFields).map((key) => (
-                    <div className="row mt-2" key={key}>
-                      <div className="col-12">
-                        <label className="label-input">
-                          {key === "email"
-                            ? "Correo electrónico"
-                            : "Contraseña"}
-                        </label>
-                        <input
-                          type={key === "password" ? "password" : "text"}
-                          placeholder={
-                            key === "email"
-                              ? "Ej: example@example.com"
-                              : "Ej: ******"
-                          }
-                          className={
-                            "form-control" +
-                            (emptyFields[key] ? " is-invalid" : "")
-                          }
-                          name={key}
-                          value={login[key]}
-                          onChange={onInputChange}
-                        />
-                        {emptyFields[key] && (
-                          <div className="invalid-feedback">
-                            Campo obligatorio
-                          </div>
-                        )}
-                      </div>
+                    <div className="d-flex justify-content-center">
+                      <button
+                        type="button"
+                        className="btn-link btn-modal-link"
+                        onClick={() => changeMode("2")}
+                      >
+                        Registrarme
+                      </button>
                     </div>
-                  ))}
-                  <div className="d-flex justify-content-center">
+                  </div>
+
+                  <div className={styles["buttones"]}>
                     <button
-                      className="btn-link btn-modal-link"
-                      onClick={(e) => changeMode("1")}
+                      type="submit"
+                      className="btn btn-modal-primary"
+                      style={{ width: "80%" }}
                     >
-                      ¿Olvidé mi password?
+                      Ingresar
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-modal-secondary"
+                      style={{ marginTop: 0, width: "80%" }}
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    >
+                      Seguir como invitado
                     </button>
                   </div>
-                  <div className="d-flex justify-content-center">
-                    <button
-                      className="btn-link btn-modal-link"
-                      onClick={(e) => changeMode("2")}
-                    >
-                      Registrarme
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className={styles["buttones"]}>
-                <button
-                  type="button"
-                  className="btn btn-modal-primary"
-                  style={{ width: "80%" }}
-                  onClick={(e) => onLogin()}
-                >
-                  Ingresar
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-modal-secondary"
-                  style={{ marginTop: 0, width: "80%" }}
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                >
-                  Seguir como invitado
-                </button>
+                </form>
               </div>
             </div>
           ) : mode == "1" ? (
             <RecuperarPassword
               onChangeMode={changeMode}
               onChangeAlert={changeAlert}
-            ></RecuperarPassword>
+            />
           ) : (
-            <Registro
-              onChangeMode={changeMode}
-              onChangeAlert={changeAlert}
-            ></Registro>
+            <Registro onChangeMode={changeMode} onChangeAlert={changeAlert} />
           )}
         </div>
       </div>
